1e3d9da7bc996fa608c0c3bc10fd40dbf9bb602b
ansarifurqaan123 <ansari.furqaan123@gmail.com>
2026-01-12 18:43:02 +0530

    first.js

--- Basic/first.js
+++ Basic/first.js
@@ -1,1 +1,10 @@
-console.log("Furqaan")
+const accountId = 826099
+let accountEmail = "furqaan007@gmail.com"
+var accountPass = "7972295773"
+accountCity = "Bhiwandi"
+let accountState;
+
+accountEmail="Furq125@gmail.com"
+
+console.log(accountId)
+console.table([accountEmail,accountId,accountPass,accountState,accountCity]);
