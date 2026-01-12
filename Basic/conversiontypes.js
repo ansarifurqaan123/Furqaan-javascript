af2f9d85da504a7881de0aecda95735f0bb6c570
ansarifurqaan123 <ansari.furqaan123@gmail.com>
2026-01-12 13:10:31 +0000

    Conversion of datatypes

---
+++ Basic/conversiontypes.js
@@ -0,0 +1,26 @@
+let score = false
+//onsole.log(typeof (score));
+
+let ConvertinNumber = Number(score);
+//console.log(typeof(ConvertinNumber));
+//console.log(ConvertinNumber);
+
+//"33"->33
+//"33abc"-> Nan
+// true->1 & false->0
+
+let AnyThing = 2
+let ConvertinBoolean = Boolean(AnyThing)
+//console.log(ConvertinBoolean);
+
+//""-> false
+//"Furqaan" bhari hoi string -> true
+// simple number ->true
+
+let Anystring = 56
+let ConvertinString = String(Anystring)
+console.log (ConvertinString);
+console.log(typeof(ConvertinString));
+
+
+
