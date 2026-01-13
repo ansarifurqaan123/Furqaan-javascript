36ffe18b05a19613773e0727cf552d1c1c7af653
ansarifurqaan123 <ansari.furqaan123@gmail.com>
2026-01-13 12:44:56 +0000

    conversiontypes

--- Basic/conversiontypes.js
+++ Basic/conversiontypes.js
@@ -19,8 +19,36 @@ let ConvertinBoolean = Boolean(AnyThing)
 
 let Anystring = 56
 let ConvertinString = String(Anystring)
-console.log (ConvertinString);
-console.log(typeof(ConvertinString));
+// console.log (ConvertinString);
+// console.log(typeof(ConvertinString));
+
+// ******************Operation**********************
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%3); 
+// // ye console.log(2%3); use hota hai reminder nikal ne ke liye ki 2 & 3 ka reminder kya hai
+// console.log(2**3);
+// // ye console.log(2**2); use hota hai power of nikal ne ke liye jaise 2^3=8 
+
+// let str1 = "Furqaan"
+// let str2 = " Ansari"
+// str3 = str1 + str2
+// console.log(str3);
+
+let x = 5   
+let y = x++
+console.log(x);
+console.log(y);
+//  do tarike ke incerment and decreamnet hote hai ek hota hai.....next line
+// prefix and postfix prefix mtlb jaise ++x phele ho ga to wo phele use kar le ga and fir dusre ko dega....
+//  postfix mtlb jaise x++ baad me ho ga to ye phele apni value dusre ko dega and fir use kare ga
+
+
+
+
+
 
 
 
