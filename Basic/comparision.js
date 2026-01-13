bd1569b19545eefaac714ce265e44bd838ce28ca
ansarifurqaan123 <ansari.furqaan123@gmail.com>
2026-01-13 13:25:02 +0000

    Comparision

---
+++ Basic/comparision.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+
+// console.log("2" > 1);
+// console.log(2 > "1");
+// is case me number khud apne aap ko number me convert kr ke compare kar dega
+
+// *****************IMP YE AESA COMPARISION KO AVOID KARNE KA JITNA HO SKHE****************
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+// is case me null 0 rhe ga is liye 12,13 line me value FALSE aai gi and 14,15 line me TRUE aai gi
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// ****(===) teen equal to bhi use hota hai compare krne ke liye......
+//  par is me wo data type ko bhi compare kre ga aise....********
+
+// console.log("2"===1); ISME FALSE aai ga q ki wo equal to data type bhi check kiya jo ge galat hai
+
+
+
+
